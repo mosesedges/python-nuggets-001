1f7bdf810bceabc6515eaca88cdc2747a9638e62
mosesedges <mosesedges@gmail.com>
2021-12-20 20:49:37 +0000

    question 5 answered

--- thirty_questions.py
+++ thirty_questions.py
@@ -49,6 +49,16 @@
 # Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
 
 
-n = int(input())
-arr = map(int, input().split())
-print(sorted(set(arr))[-2])
+# n = int(input())
+# arr = map(int, input().split())
+# print(sorted(set(arr))[-2])
+
+
+arr = [2, 4, 5, 5, 7, 9, 12, 18, 90, 38, 64, 51, 83]
+
+maxnum = sorted(set(arr))[-1]
+
+
+minnum = sorted(set(arr))[0]
+
+print(sorted(set(arr)), maxnum, minnum)
