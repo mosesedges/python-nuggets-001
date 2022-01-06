071de67bc70ed4367013706b21b4143084c7b322
mosesedges <mosesedges@gmail.com>
2022-01-06 21:01:54 +0000

    Q 9  added

--- thirty_questions.py
+++ thirty_questions.py
@@ -170,3 +170,20 @@ def swap_case(s):
 s = input()
 result = swap_case(s)
 print(result)
+
+
+# ****** SOLUTION 9 *****
+
+# You are given a positive integer . Print a numerical triangle of height  like the one below:
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# ......
+# Can you do it using only arithmetic operations, a single for loop and print statement?
+
+# Use no more than two lines. The first line (the for statement) is already written for you. You have to complete the print statement.
+
+# Note: Using anything related to strings will give a score of .
