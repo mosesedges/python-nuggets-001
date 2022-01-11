2937639cc728f81413665163a54caed969c11923
mosesedges <mosesedges@gmail.com>
2022-01-11 00:04:05 +0000

    Q 9  added

---
+++ try.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+# time = datetime.strptime(input(), "%H:%M")
+
+# print("%s:%s" % (time.hour, time.minute))
+
+
+# now = datetime.now()  # current date and time
+# year = now.strftime("%Y")
+# print(year)
+
+# month = now.strftime("%m")
+# print(month)
+
+# day = now.strftime("%d")
+# print(day)
+
+# time = now.strftime("%H:%M:%S")
+# print(" + (str(time) + ")
+
+# date = now.strftime("%Y-%m-%d %H:%M:%S")
+# print(date)
+
+
+# import json
+# List = ["A", "B"]
+# print json.dumps(List)
+# ["A", "B"]
+
+
+# sale = {input(): int(input())}
+# print(sale)
+
+
+# def work(shift) -> str:
+#     sale = {input(): int(input())}
+#     shift("make")
+#     return sale
+
+arr = [1, 2, 3, 6]
+
+print("".join(map(str, arr[::-1])))
